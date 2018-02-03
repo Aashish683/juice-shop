7a7caad84dbf59591e111ddade5899e23f57188a
Aashish683 <f2016683@pialni.bits-pilani.ac.in>
2018-02-04 04:15:45 +0530

    display reviews on dialog

--- Angular5Upgrade/src/app/display-product/display-product.component.ts
+++ Angular5Upgrade/src/app/display-product/display-product.component.ts
@@ -1,5 +1,7 @@
+import { ProductReviewService } from './../Services/product-review.service';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-display-product',
@@ -8,9 +10,12 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 })
 export class DisplayProductComponent implements OnInit {
 
+  reviews$:Observable<string[]>;
   constructor(private dialogRef: MatDialogRef<DisplayProductComponent>,
-    @Inject(MAT_DIALOG_DATA) private data: any) {
+    @Inject(MAT_DIALOG_DATA) private data: any,private productReviewServe:ProductReviewService) {
       this.data=this.data.productData;
+      this.reviews$=this.productReviewServe.get(this.data.id);
+      this.reviews$.subscribe(x=>console.log(x))
     }
 
   ngOnInit(){

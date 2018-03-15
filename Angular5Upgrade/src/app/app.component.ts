5c73236e06b16841d5865e748fe12d05989f0bf5
Aashish683 <aashishsingh2907@gmail.com>
2018-03-15 18:12:14 +0530

    minor changes

--- Angular5Upgrade/src/app/app.component.ts
+++ Angular5Upgrade/src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent {
   }
 
   test(){
-    this.testServe.testConfiguration().subscribe((confData)=>{
+    this.testServe.testConfiguration().subscribe((confData:any)=>{
       console.log(confData);
       if (confData && confData.application && confData.application.name !== null) {
         this.applicationName = confData.application.name
